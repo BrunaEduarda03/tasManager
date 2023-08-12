d00d4128305e96a3502b8c2864c7bb8b90ca900a
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-08-12 11:50:15 -0300

    feat:fetch tasks from backend

--- gerenciador-react/src/App.js
+++ gerenciador-react/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import TaskItem from "./components/TaskItem";
+import axios from "axios";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:8000/tasks");
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleClick = () => {
+    setTasks([]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {tasks.map((task) => (
+        <TaskItem key={task.id} task={task} />
+      ))}
+      <button onClick={handleClick}>Limpar Tarefas</button>
     </div>
   );
 }
